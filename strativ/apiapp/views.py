571d7411856f565d508c033e6583f8bdf8755036
Ashraf <airriaislam@gmail.com>
2021-08-19 02:35:50 +0600

    A simple view is created for checking if the response is working or not. Also the models for storing the response in database has been created and migrated.

--- strativ/apiapp/views.py
+++ strativ/apiapp/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
+import requests
+
 
 # Create your views here.
+def index(request):
+    responses = requests.get('https://restcountries.eu/rest/v2/all').json()
 
+    context = {
+        'responses': responses
+    }
+
+    return render(request, "apiapp/index.html", context)
